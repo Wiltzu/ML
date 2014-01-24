6329f60b5df23498491d4514ff85a221efee228a
Wiltzu <ville.ahti2@gmail.com>
2014-01-24 12:20:34 +0000

    tried to make regularized linear regression

--- lin_reg.py
+++ lin_reg.py
@@ -2,23 +2,49 @@ from numpy import *
 
 def lin_reg(X, y):
 	m, n = X.shape
-	learn_rate = 0.001
+	learn_rate = 0.000001
 	weights = ones(n + 1)
 	X = hstack((ones((m, 1)), X))
 	print weights
 	print X
 
-	predictions = dot(X, weights)
+	predictions = predict(X, weights)
 	print predictions
 	
 	for i in range(20):
 		print 'iteration', i
 		weights -= learn_rate * (1.0/m) * dot(predictions - y,X)
 		print 'weights =', weights
-		predictions = dot(X, weights)
+		predictions = predict(X, weights)
+		print 'predictions =', predictions
+
+def regularized_lin_reg(X, y):
+	m, n = X.shape
+	learn_rate = 0.000001
+	regularization_param = 5
+	weights = ones(n + 1)
+	X = hstack((ones((m, 1)), X))
+	print weights
+	print X
+
+	predictions = predict(X, weights)
+	print predictions
+	
+	for i in range(20):
+		print 'iteration', i
+		weights -= learn_rate * (1.0/m) * (dot(predictions - y,X) + ((regularization_param/m) * weights)) 
+		print 'weights =', weights
+		predictions = predict(X, weights)
 		print 'predictions =', predictions
 
 
-		
 
-lin_reg(arange(8).reshape(4,2), [0,2,4,8])
+def predict(X, weights):
+	return dot(X, weights)
+		
+X = arange(512).reshape(32,16) 
+y = [i for i in range(32)]
+print "linear regression:"
+lin_reg(X, y)
+print "regularized linear regression:"
+regularized_lin_reg(X,y)
